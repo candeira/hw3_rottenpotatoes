5d8bbf28a3b86474ed3b1bd7f382595380e28661
Javier Candeira <javier@candeira.com>
2012-03-19 05:29:39 -0700

    Create a declarative scenario step for adding movies

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -4,8 +4,9 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    new_movie = Movie.create!(movie)
+    assert Movie.find_by_id(new_movie[:id]) == new_movie
   end
-  assert false, "Unimplmemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
